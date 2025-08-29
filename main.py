aa853be6b4bf8fe0b3d48cc1c9f13f738d18b885
Pedro Lucas <pedrolucasgonza31@gmail.com>
2025-08-29 20:05:24 -0300

    up

--- main.py
+++ main.py
@@ -4,4 +4,6 @@ app = FastAPI(title="Meu App")
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="
+    uvicorn.run(app, host="0.0.0.0", port=8000, log_level="info")
+
+    
